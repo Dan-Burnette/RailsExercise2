94fb25c76081904f6b6194858912e25ac8f05ce8
Daniel Burnette <dcburnette999@gmail.com>
2014-10-16 12:47:21 -0500

    added error displaying to a failed user creation attempt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if (user.save)
+    @user = User.new(user_params)
+    @users = User.all
+    if (@user.save)
       redirect_to users_path
     else
       render "index"
